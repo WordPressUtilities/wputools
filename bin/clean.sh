c39dbac85eff1542d7e992ba339dc4b5a68fbe9a
Kevin <kevin@rocher-dev.com>
2019-07-06 21:59:11 +0200

    v 0.3.3
    * Clean : Ask for a backup.
    * Clean : Delete unapproved comments.
    * Clean : Delete invalid term_taxonomy table content.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 ###################################
+## Ask for a backup
+###################################
+
+read -p "Do you need a backup ? [Y,n] : " wputools_clean_need_backup
+if [[ "${wputools_clean_need_backup}" != "N" && "${wputools_clean_need_backup}" != "n" ]]; then
+    wp db export;
+fi
+
+###################################
 ## Database
 ###################################
 
-## Delete useless fields
+## Various
 ###################################
 
 # Delete SPAMs
 wp comment delete --force $(wp comment list --status=spam --field=ID);
+
+# Unapproved comments
+wp db query "DELETE from $(wp db prefix)comments WHERE comment_approved = '0';";
 
 # Delete revisions
 wp post delete --force $(wp post list --post_type='revision' --format=ids);
@@ -16,9 +28,16 @@ wp post delete --force $(wp post list --post_type='revision' --format=ids);
 # Delete locks
 wp db query "DELETE FROM $(wp db prefix)postmeta WHERE meta_key IN ('_edit_lock','_edit_last')";
 
+## Terms
+###################################
+
 # Delete empty terms
 wp db query "DELETE FROM $(wp db prefix)terms WHERE term_id IN (SELECT term_id FROM $(wp db prefix)term_taxonomy WHERE count = 0 )";
+wp db query "DELETE FROM $(wp db prefix)term_taxonomy WHERE term_id not IN (SELECT term_id FROM $(wp db prefix)terms);";
 wp db query "DELETE FROM $(wp db prefix)term_relationships WHERE term_taxonomy_id not IN (SELECT term_taxonomy_id FROM $(wp db prefix)term_taxonomy)";
+
+## Metas
+###################################
 
 # Delete orphaned post metas
 wp db query "DELETE pm FROM $(wp db prefix)postmeta pm LEFT JOIN $(wp db prefix)posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL";
