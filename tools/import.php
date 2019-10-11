f90d79b48646d881e156eab6be24fb3bb1503c6c
Kevin <kevin@rocher-dev.com>
2019-10-11 23:41:33 +0200

    v 0.6.1
    * Import : handle metas & terms

--- tools/import.php
+++ tools/import.php
@@ -27,11 +27,36 @@ class wputools_import extends WPUWooImportExport {
         /* Parse datas */
         $nb_posts = count($datas);
         foreach ($datas as $i => $data) {
+
             $ii = $i + 1;
             $total = "${ii} / {$nb_posts} - ";
 
+            if (!isset($data['metas'])) {
+                $data['metas'] = array();
+            }
+            if (!isset($data['terms'])) {
+                $data['terms'] = array();
+            }
+
+            foreach ($data as $key => $value) {
+                if (substr($key, 0, 6) == 'meta__') {
+                    $data['metas'][substr($key, 6)] = $value;
+                }
+                if (substr($key, 0, 6) == 'term__') {
+                    $data['terms'][substr($key, 6)] = $value;
+                }
+            }
+
+            if(isset($data['uniqid'])){
+                $data['metas']['uniqid'] = $data['uniqid'];
+                $post_id = $this->create_or_update_post_from_datas($data, array(
+                    'uniqid' => $data['uniqid']
+                ));
+            }
+            else {
+                $post_id = $this->create_post_from_data($data);
+            }
             /* Create post */
-            $post_id = $this->create_post_from_data($data);
             if (is_numeric($post_id)) {
                 $this->print_message($total . "Post ID #${post_id} created !");
             } else {
