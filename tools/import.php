fe17a3849521f4dca3472150575426e9d350dc91
Kevin <kevin@rocher-dev.com>
2019-10-12 16:53:19 +0200

    v 0.6.2
    * Import - use an existing function.

--- tools/import.php
+++ tools/import.php
@@ -16,54 +16,12 @@ class wputools_import extends WPUWooImportExport {
             $this->print_message("No valid file provided");
             return;
         }
+        $this->sync_posts_from_csv($argv[1], array(
+            'debug_type' => 'print'
+        ));
 
-        /* Try to extract datas */
-        $datas = $this->get_datas_from_csv($argv[1]);
-        if (!is_array($datas)) {
-            $this->print_message("Invalid valid file provided");
-            return;
-        }
-
-        /* Parse datas */
-        $nb_posts = count($datas);
-        foreach ($datas as $i => $data) {
-
-            $ii = $i + 1;
-            $total = "${ii} / {$nb_posts} - ";
-
-            if (!isset($data['metas'])) {
-                $data['metas'] = array();
-            }
-            if (!isset($data['terms'])) {
-                $data['terms'] = array();
-            }
-
-            foreach ($data as $key => $value) {
-                if (substr($key, 0, 6) == 'meta__') {
-                    $data['metas'][substr($key, 6)] = $value;
-                }
-                if (substr($key, 0, 6) == 'term__') {
-                    $data['terms'][substr($key, 6)] = $value;
-                }
-            }
-
-            if(isset($data['uniqid'])){
-                $data['metas']['uniqid'] = $data['uniqid'];
-                $post_id = $this->create_or_update_post_from_datas($data, array(
-                    'uniqid' => $data['uniqid']
-                ));
-            }
-            else {
-                $post_id = $this->create_post_from_data($data);
-            }
-            /* Create post */
-            if (is_numeric($post_id)) {
-                $this->print_message($total . "Post ID #${post_id} created !");
-            } else {
-                $this->print_message($total . "Post could not be created");
-            }
-        }
     }
+
 }
 
 new wputools_import();
