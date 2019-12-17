5270bdbad59199d2aa39a7a8fbb19a528e50aa7e
Kevin <kevin@rocher-dev.com>
2019-12-17 11:21:09 +0100

    v 0.8.3
    * BDUser : Fix default login menu

--- bin/bduser.sh
+++ bin/bduser.sh
@@ -21,7 +21,7 @@ cp "${_TOOLSDIR}SecuPress-Backdoor-User/secupress-backdoor-user.php" "${_BD_PATH
 bashutilities_sed "s/get_users()/get_users(array('role'=>'administrator'))/g" "${_BD_PATH}";
 
 # Default menu is login
-bashutilities_sed "s/\ \:\ \'dash\'/\ \:\ \'read\'/g" "${_BD_PATH}";
+bashutilities_sed "s/'dash';/'read';/g" "${_BD_PATH}";
 
 # File will be deleted after use so lets ensure rights are ok.
 chmod 0777 "${_BD_PATH}";
