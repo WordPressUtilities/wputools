034710e25a59958bc2331f0663024a1494d24e45
Kevin <kevin@rocher-dev.com>
2020-04-09 21:28:59 +0200

    v 0.16.2
    * Plugin : install plugins from the WP Repository.
    * Mu-plugin : install only from whitelist.

--- bin/muplugin.sh
+++ bin/muplugin.sh
@@ -3,6 +3,20 @@
 echo "# INSTALL MU-PLUGIN";
 
 _MUPLUGIN_ID="${1}";
+_PLUGIN_ID="${1}";
+_PLUGIN_LIST=($(cat ${_WPUTOOLS_MUPLUGIN_LIST} | tr "\n" " " ));
+_IS_WPU='0';
+for plugin_item in "${_PLUGIN_LIST[@]}"; do
+    if [[ "${_PLUGIN_ID}" == "${plugin_item}" ]];then
+        echo "${plugin_item} present in the array";
+        _IS_WPU="1";
+    fi;
+done
+
+if [[ "${_IS_WPU}" != '1' ]];then
+    echo $(bashutilities_message "The mu-plugin \"${_MUPLUGIN_ID}\" is not a WordPressUtilities mu-plugin." 'error');
+    return;
+fi;
 
 # Install plugin
 cd "${_CURRENT_DIR}wp-content/mu-plugins/wpu/";
--- bin/plugin.sh
+++ bin/plugin.sh
@@ -3,21 +3,33 @@
 echo "# INSTALL PLUGIN";
 
 _PLUGIN_ID="${1}";
+_PLUGIN_LIST=($(cat ${_WPUTOOLS_PLUGIN_LIST} | tr "\n" " " ));
+_IS_WPU='0';
+for plugin_item in "${_PLUGIN_LIST[@]}"; do
+    if [[ "${_PLUGIN_ID}" == "${plugin_item}" ]];then
+        echo "${plugin_item} present in the array";
+        _IS_WPU="1";
+    fi;
+done
 
-# Install plugin
-cd "${_CURRENT_DIR}wp-content/plugins/";
-git submodule add --force "https://github.com/WordPressUtilities/${_PLUGIN_ID}.git";
+# Install plugin and activate
+if [[ "${_IS_WPU}" == '1' ]];then
+    cd "${_CURRENT_DIR}wp-content/plugins/";
+    git submodule add --force "https://github.com/WordPressUtilities/${_PLUGIN_ID}.git";
+    cd "${_CURRENT_DIR}";
+    _WPCLICOMMAND plugin activate "${1}";
+else
+    _WPCLICOMMAND plugin install --activate "${1}";
+fi
 
-# Activate and get infos
-_WPCLICOMMAND plugin activate "${1}";
+# Get infos
 _PLUGIN_VERSION=$(_WPCLICOMMAND plugin get "${_PLUGIN_ID}" --field=version);
 _PLUGIN_TITLE=$(_WPCLICOMMAND plugin get "${_PLUGIN_ID}" --field=title);
 
 # Commit items
-cd "${_CURRENT_DIR}";
 git reset;
 git add "${_CURRENT_DIR}wp-content/plugins/${_PLUGIN_ID}/";
 git add .gitmodules;
 
 # Commit
-git commit -m "Add Plugin : ${_PLUGIN_TITLE} v ${_PLUGIN_VERSION}";
+git commit --no-verify -m "Add Plugin : \"${_PLUGIN_TITLE}\" v ${_PLUGIN_VERSION}";
