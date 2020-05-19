4bc1b23106ef5f644b421b654b8c58ff7fb89eae
Kevin <kevin@rocher-dev.com>
2020-05-19 23:06:13 +0200

    v 0.19.2
    * Add diff files when testing before/after an update.

--- inc/functions.sh
+++ inc/functions.sh
@@ -12,10 +12,12 @@ if [[ -f "${_CURRENT_DIR}wputools-urls.txt" ]];then
     _wputools_test__file="${_CURRENT_DIR}wputools-urls.txt";
 fi;
 
+_wputools_test_before_content='';
+_wputools_test_after_content='';
 _wputools_test_before_length=0;
 _wputools_test_after_length=0;
 
-function check_urls(){
+function wputools_test_check_urls(){
     WPUTools cache > /dev/null;
     for line in $(cat "${_wputools_test__file}")
     do
@@ -26,20 +28,22 @@ function check_urls(){
 function run_test_before(){
     if [[ -f "${_wputools_test__file}" ]];then
         echo '# RUNNING TESTS : BEFORE ACTION';
-        _check_url_before=$(check_urls);
-        _wputools_test_before_length=${#_check_url_before};
+        _wputools_test_before_content=$(wputools_test_check_urls);
+        _wputools_test_before_length=${#_wputools_test_before_content};
     fi;
 }
 
 function run_test_after(){
     if [[ -f "${_wputools_test__file}" ]];then
         echo '# RUNNING TESTS : AFTER ACTION';
-        _check_url_after=$(check_urls);
-        _wputools_test_after_length=${#_check_url_after};
+        _wputools_test_after_content=$(wputools_test_check_urls);
+        _wputools_test_after_length=${#_wputools_test_after_content};
         if [[ "${_wputools_test_before_length}" != "${_wputools_test_after_length}" ]];then
             echo "Content size has changed on tested URLs :";
             echo "- Before: ${_wputools_test_before_length}";
             echo "- After: ${_wputools_test_after_length}";
+            echo "${_wputools_test_before_content}" > diff-before.txt;
+            echo "${_wputools_test_after_content}" > diff-after.txt;
         fi
     fi;
 }
