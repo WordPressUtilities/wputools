6b95f99204e89d586cfcb8cb0bee42f01a7b30f9
Kevin <kevin@rocher-dev.com>
2020-05-24 19:13:49 +0200

    v 0.19.4
    * Update WPUWOO.
    * Settings : Add url files.

--- bin/settings.sh
+++ bin/settings.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
 ###################################
-## Check
-###################################
-
-if [[ "${_HAS_WPUTOOLS_LOCAL}" == '1' ]];then
-    echo $(bashutilities_message "A wputools-local.sh file already exists" 'warning');
-    return;
-fi
-
-###################################
 ## Asks
 ###################################
 
 _WPUTOOLS_LOCAL_PATH='-';
-_WPUTOOLS_LOCAL_PATH_ASK=$(bashutilities_get_yn "- Create the wputools-local.sh file one level over the root of your WordPress install ?" 'y');
+_WPUTOOLS_LOCAL_PATH_ASK=$(bashutilities_get_yn "- Create the config files one level over the root of your WordPress install ?" 'y');
 if [[ "${_WPUTOOLS_LOCAL_PATH_ASK}" == 'y' ]];then
-    _WPUTOOLS_LOCAL_PATH="${_CURRENT_DIR}../wputools-local.sh";
+    _WPUTOOLS_LOCAL_PATH="${_CURRENT_DIR}../";
 else
-    _WPUTOOLS_LOCAL_PATH_ASK=$(bashutilities_get_yn "- Create the wputools-local.sh file at the root of your WordPress install ?" 'y');
+    _WPUTOOLS_LOCAL_PATH_ASK=$(bashutilities_get_yn "- Create the config files at the root of your WordPress install ?" 'y');
     if [[ "${_WPUTOOLS_LOCAL_PATH_ASK}" == 'y' ]];then
-        _WPUTOOLS_LOCAL_PATH="${_CURRENT_DIR}wputools-local.sh";
+        _WPUTOOLS_LOCAL_PATH="${_CURRENT_DIR}";
     fi;
 fi;
 
@@ -37,10 +28,25 @@ fi
 ## Create
 ###################################
 
-cp "${_TOOLSDIR}wputools-local.sh" "${_WPUTOOLS_LOCAL_PATH}";
-
 wputools_use_home_url=$(bashutilities_get_yn "- Use “${_HOME_URL}” as home_url?" 'y');
-if [[ "${wputools_use_home_url}" == 'y' ]];then
-    bashutilities_sed "s#http://example.com#${_HOME_URL}#g" "${_WPUTOOLS_LOCAL_PATH}";
-    bashutilities_sed "s/#_HOME_URL/_HOME_URL/g" "${_WPUTOOLS_LOCAL_PATH}";
+
+if [[ "${_HAS_WPUTOOLS_LOCAL}" == '1' ]];then
+    echo $(bashutilities_message "A wputools-local.sh file already exists" 'warning');
+else
+    _WPUTOOLS_LOCAL_FILE="${_WPUTOOLS_LOCAL_FILE}wputools-local.sh";
+    cp "${_TOOLSDIR}wputools-local.sh" "${_WPUTOOLS_LOCAL_FILE}";
+    if [[ "${wputools_use_home_url}" == 'y' ]];then
+        bashutilities_sed "s#http://example.com#${_HOME_URL}#g" "${_WPUTOOLS_LOCAL_FILE}";
+        bashutilities_sed "s/#_HOME_URL/_HOME_URL/g" "${_WPUTOOLS_LOCAL_FILE}";
+    fi
+fi
+
+if [[ "${_wputools_test__file}" != '' ]];then
+    echo $(bashutilities_message "A wputools-urls.txt file already exists" 'warning');
+else
+    _WPUTOOLS_URL_LOCAL_FILE="${_WPUTOOLS_LOCAL_PATH}wputools-urls.txt";
+    touch "${_WPUTOOLS_URL_LOCAL_FILE}";
+    if [[ "${wputools_use_home_url}" == 'y' ]];then
+        echo "${_HOME_URL}" > "${_WPUTOOLS_URL_LOCAL_FILE}";
+    fi
 fi
