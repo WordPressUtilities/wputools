eec0126ecae0e8e5aa67daccdc99e9fff8f1fc38
Kevin <kevin@rocher-dev.com>
2020-06-07 19:45:49 +0200

    v 0.19.7
    * Import uploads.

--- bin/dbimport.sh
+++ bin/dbimport.sh
@@ -53,6 +53,40 @@ if [[ -n "${_WPDB_REPLACE_BEFORE}" && -n "${_WPDB_REPLACE_AFTER}" ]];then
     _WPCLICOMMAND search-replace "${_WPDB_REPLACE_BEFORE}" "${_WPDB_REPLACE_AFTER}";
 fi;
 
+# Check if uploads can be retrieved
+uploads_dir='-';
+dbimport_uploads='n';
+if [[ -d "${_tmp_folder}" ]];then
+    # Check if uploads are available aside backup
+    dirname_file=$(dirname "${_dbimport_file}");
+    uploads_dir="${dirname_file}/uploads";
+    # Ask to import uploads
+    if [[ "${uploads_dir}" != '-' && -d "${uploads_dir}" ]];then
+        dbimport_uploads=$(bashutilities_get_yn "- Import uploads ?" 'y');
+    fi
+fi;
+
+if [[ "${dbimport_uploads}" == 'y' ]];then
+    # Move away current uploads
+    dbimport_uploads_current="${_CURRENT_DIR}wp-content/uploads";
+    dbimport_uploads_current_rand=$(bashutilities_rand_string 6);
+    dbimport_uploads_current_new="${dbimport_uploads_current}-${dbimport_uploads_current_rand}";
+    if [[ -d "${dbimport_uploads_current}" ]];then
+        mv "${dbimport_uploads_current}" "${dbimport_uploads_current_new}";
+    fi;
+
+    # Move uploads dir to current upload path
+    mv "${uploads_dir}" "${dbimport_uploads_current}";
+    echo "# Uploads imported"
+
+    # Ask to delete old uploads
+    dbimport_uploads_delete_current=$(bashutilities_get_yn "- Delete old uploads ?" 'y');
+    if [[ "${dbimport_uploads_delete_current}" == 'y' ]];then
+        rm -rf "${dbimport_uploads_current_new}";
+        echo "# Old Uploads deleted";
+    fi;
+fi;
+
 # Disable maintenance mode
 _WPCLICOMMAND maintenance-mode deactivate;
 
