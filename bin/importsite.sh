f0697d1fa7c353f5b26e828b8097359bb727eef8
Kevin <kevin@rocher-dev.com>
2020-11-22 17:48:30 +0100

    v 0.25.1
    * Extract MySQL access from wp-config.php.

--- bin/importsite.sh
+++ bin/importsite.sh
@@ -10,7 +10,7 @@ _WPUTOOLS_IMPORT_USER="";
 _WPUTOOLS_IMPORT_PASS="";
 _WPUTOOLS_IMPORT_PATH="~/www/";
 
-# Database
+# Database ( Can be extracted from wp-config.php )
 _WPUTOOLS_IMPORT_DB_NAME="";
 _WPUTOOLS_IMPORT_DB_USER="";
 _WPUTOOLS_IMPORT_DB_PASSWORD="";
@@ -97,14 +97,41 @@ if [[ ! -d "htdocs/wp-content/uploads" ]];then
 fi;
 
 ###################################
+## Extract from wp-config
+###################################
+
+_config_file="htdocs/wp-config.php";
+if [[ -f "${_config_file}" ]];then
+    _TMP_DB_NAME=$(bashutilities_search_extract_file__php_constant "DB_NAME" "${_config_file}");
+    _TMP_DB_USER=$(bashutilities_search_extract_file__php_constant "DB_USER" "${_config_file}");
+    _TMP_DB_PASSWORD=$(bashutilities_search_extract_file__php_constant "DB_PASSWORD" "${_config_file}");
+    _TMP_DB_HOST=$(bashutilities_search_extract_file__php_constant "DB_HOST" "${_config_file}");
+
+    if [[ -n "${_TMP_DB_NAME}" && -n "${_TMP_DB_USER}" && -n "${_TMP_DB_PASSWORD}" && -n "${_TMP_DB_HOST}" ]];then
+        echo "NAME : ${_TMP_DB_NAME}";
+        echo "USER : ${_TMP_DB_USER}";
+        echo "PASSWORD : ${_TMP_DB_PASSWORD}";
+        echo "HOST : ${_TMP_DB_HOST}";
+        _use_tmp_values=$(bashutilities_get_yn "- Use temporary MySQL values?" 'y');
+        if [[ "${_use_tmp_values}" == 'y' ]];then
+            _WPUTOOLS_IMPORT_DB_NAME="${_TMP_DB_NAME}";
+            _WPUTOOLS_IMPORT_DB_USER="${_TMP_DB_USER}";
+            _WPUTOOLS_IMPORT_DB_PASSWORD="${_TMP_DB_PASSWORD}";
+            _WPUTOOLS_IMPORT_DB_HOST="${_TMP_DB_HOST}";
+        fi;
+    fi;
+fi;
+
+###################################
 ## Import database
 ###################################
 
-if [[ ! -z "${_WPUTOOLS_IMPORT_HOST}" && ! -z "${_WPUTOOLS_IMPORT_USER}" && ! -z "${_WPUTOOLS_IMPORT_PASS}" && ! -z "${_WPUTOOLS_IMPORT_PATH}" ]]; then
-    if [[ ! -f "dblocal.sql.gz" ]];then
-        echo "# Importing database"
+_dump_filename="dblocal.sql.gz";
+if [[ ! -z "${_WPUTOOLS_IMPORT_HOST}" && ! -z "${_WPUTOOLS_IMPORT_USER}" && ! -z "${_WPUTOOLS_IMPORT_PASS}" && ! -z "${_WPUTOOLS_IMPORT_DB_NAME}" ]]; then
+    if [[ ! -f "${_dump_filename}" ]];then
+        echo "# Importing database";
         ssh "${_WPUTOOLS_IMPORT_USER}"@"${_WPUTOOLS_IMPORT_HOST}" \
-            "mysqldump -h ${_WPUTOOLS_IMPORT_DB_HOST} -u ${_WPUTOOLS_IMPORT_DB_USER} -p${_WPUTOOLS_IMPORT_DB_PASSWORD} ${_WPUTOOLS_IMPORT_DB_NAME} | gzip -9" > dblocal.sql.gz
+            "mysqldump -h ${_WPUTOOLS_IMPORT_DB_HOST} -u ${_WPUTOOLS_IMPORT_DB_USER} -p${_WPUTOOLS_IMPORT_DB_PASSWORD} ${_WPUTOOLS_IMPORT_DB_NAME} | gzip -9" > "${_dump_filename}";
     else
         echo "# Dump already exists";
     fi;
