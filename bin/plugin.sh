64d8c6842e3407e4a9159b47878204c3e4e0f008
Kevin <kevin@rocher-dev.com>
2021-03-15 21:41:41 +0100

    v 0.28.1
    * Plugin install security.

--- bin/plugin.sh
+++ bin/plugin.sh
@@ -16,6 +16,10 @@ for plugin_item in "${_PLUGIN_LIST[@]}"; do
     fi;
 done
 
+# Base vars
+_PLUGIN_DIR="${_CURRENT_DIR}wp-content/plugins/${_PLUGIN_ID}/";
+_PLUGIN_LANG_DIR="${_CURRENT_DIR}wp-content/languages/plugins/${_PLUGIN_ID}";
+
 # Install plugin and activate
 if [[ "${_IS_WPU}" == '1' ]];then
     cd "${_CURRENT_DIR}wp-content/plugins/";
@@ -26,6 +30,11 @@ else
     _WPCLICOMMAND plugin install --activate "${1}";
 fi
 
+if [[ ! -d "${_PLUGIN_DIR}" ]];then
+    bashutilities_message 'Plugin could not be installed' 'error';
+    return 0;
+fi;
+
 _PLUGIN_LANG=$(_WPCLICOMMAND language core list --field=language --status=active);
 _WPCLICOMMAND language plugin install "${_PLUGIN_ID}" "${_PLUGIN_LANG}";
 
@@ -35,9 +44,13 @@ _PLUGIN_TITLE=$(_WPCLICOMMAND plugin get "${_PLUGIN_ID}" --field=title);
 
 # Commit items
 git reset;
-git add "${_CURRENT_DIR}wp-content/plugins/${_PLUGIN_ID}/";
-git add "${_CURRENT_DIR}wp-content/languages/plugins/${_PLUGIN_ID}*";
-git add .gitmodules;
+git add "${_PLUGIN_DIR}";
+if [[ -d "${_PLUGIN_LANG_DIR}" ]];then
+    git add "${_PLUGIN_LANG_DIR}*";
+fi;
+if [[ -f "${_CURRENT_DIR}.gitmodules" ]];then
+    git add  "${_CURRENT_DIR}.gitmodules";
+fi;
 
 # Commit
 git commit --no-verify -m "Add Plugin : \"${_PLUGIN_TITLE}\" v ${_PLUGIN_VERSION}";
