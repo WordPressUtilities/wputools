d9891168dfb82081b420c6293b3ff054e4c76743
Kevin <kev.rocher@gmail.com>
2024-10-05 22:58:46 +0200

    v 0.101.1
    Login now bypasses WP-2FA

--- tools/login.php
+++ tools/login.php
@@ -70,6 +70,7 @@ if (!$user) {
 
 /* Disable two-factor auth */
 add_filter('two_factor_providers', '__return_empty_array', 10, 1);
+add_filter('wp_2fa_user_enabled_methods', '__return_empty_array', 10, 1);
 
 /* Avoid unwanted redirects or else */
 remove_all_actions('wp_logout');
