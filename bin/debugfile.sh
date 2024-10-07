49f8682ae2e16b05b16bab4196dba375830a34a8
Kevin <kev.rocher@gmail.com>
2024-10-07 10:28:26 +0200

    v 0.101.2
    - Restore useful vars.
    - Debugfile : more infos on why debug file is not available.

--- bin/debugfile.sh
+++ bin/debugfile.sh
@@ -6,7 +6,16 @@ function wputools__debug_display_file() {
     _WPCLICOMMAND eval "error_log('eval_start');";
     local _debug_file=$(_WPCLICOMMAND config get "WP_DEBUG_LOG");
     if [[ ! -f "${_debug_file}" ]];then
-        echo "No debug file";
+
+        # check if log directory exists
+        local _debug_dir=$(dirname "${_debug_file}");
+        if [[ ! -d "${_debug_dir}" ]];then
+            echo "-> Log directory does not exists : ${_debug_dir}";
+            return;
+        fi;
+
+        echo "-> Debug file does not exists : ${_debug_file}";
+
         return;
     fi;
     bashutilities_message 'Start displaying debug file log' 'success';
