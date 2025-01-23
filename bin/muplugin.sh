c95dc3738c66591e560d35b76b30a878a27d112c
Kevin <kevin@rocher-dev.com>
2025-01-23 22:35:21 +0100

    v 0.112.2
    mu plugin : check folder & base install before adding an mu-plugin.

--- bin/muplugin.sh
+++ bin/muplugin.sh
@@ -4,11 +4,27 @@ echo "# INSTALL MU-PLUGIN";
 
 function wputools__muplugin(){
     local _MUPLUGIN_ID="${1}";
-    local _PLUGIN_DIR="${_CURRENT_DIR}wp-content/plugins/${_MUPLUGIN_ID}/";
-    local _MUPLUGIN_DIR="${_CURRENT_DIR}wp-content/mu-plugins/wpu/${_MUPLUGIN_ID}/";
+    local _PLUGIN_ROOT="${_CURRENT_DIR}wp-content/plugins/";
+    local _MUPLUGIN_ROOT="${_CURRENT_DIR}wp-content/mu-plugins/";
+    local _PLUGIN_DIR="${_PLUGIN_ROOT}${_MUPLUGIN_ID}/";
+    local _MUPLUGIN_DIR="${_MUPLUGIN_ROOT}wpu/${_MUPLUGIN_ID}/";
     if [[ "${_MUPLUGIN_ID}" == '' ]];then
         bashutilities_message "No mu-plugin specified." 'error';
         return 0;
+    fi
+
+    if [[ ! -d "${_MUPLUGIN_ROOT}" ]];then
+        mkdir -p "${_MUPLUGIN_ROOT}";
+    fi
+    if [[ ! -f "${_MUPLUGIN_ROOT}wpu_muplugin_autoloader.php" ]];then
+        local _URL_AUTOLOADER="https://raw.githubusercontent.com/Darklg/WPUtilities/refs/heads/master/wp-content/mu-plugins/wpu_muplugin_autoloader.php";
+        curl -o "${_MUPLUGIN_ROOT}wpu_muplugin_autoloader.php" "${_URL_AUTOLOADER}";
+        git reset;
+        git add "${_MUPLUGIN_ROOT}wpu_muplugin_autoloader.php";
+        git commit -m "Add MU-Plugin : wpu_muplugin_autoloader";
+    fi
+    if [[ ! -d "${_MUPLUGIN_ROOT}wpu/" ]];then
+        mkdir -p "${_MUPLUGIN_ROOT}wpu/";
     fi
 
     if [[ -d "${_MUPLUGIN_DIR}" || -d "${_PLUGIN_DIR}" ]];then
