6b296636d687dac3767bf056c3cf484d88cb460b
Kevin <kevin@rocher-dev.com>
2026-04-07 17:09:59 +0200

    v 0.160.0
    mu : try to install from github

--- bin/muplugin.sh
+++ bin/muplugin.sh
@@ -40,14 +40,26 @@ function wputools__muplugin(){
         fi;
     done
 
+    local _MUPLUGIN_REPO_URL="https://github.com/WordPressUtilities/${_MUPLUGIN_ID}";
     if [[ "${_IS_WPU}" != '1' ]];then
         bashutilities_message "The mu-plugin \"${_MUPLUGIN_ID}\" is not a WordPressUtilities mu-plugin." 'error';
-        return;
+        if [[ $(bashutilities_get_yn "Check if the repo exists at ${_MUPLUGIN_REPO_URL} ?" 'y') == 'n' ]];then
+            return;
+        fi;
+        local _REPO_STATUS=$(curl -s -o /dev/null -w "%{http_code}" "${_MUPLUGIN_REPO_URL}");
+        if [[ "${_REPO_STATUS}" != '200' ]];then
+            bashutilities_message "The repo does not exist." 'error';
+            return;
+        fi;
+        bashutilities_message "The repo exists!" 'success';
+        if [[ $(bashutilities_get_yn "Continue with this repo ?" 'y') != 'y' ]];then
+            return;
+        fi;
     fi;
 
     # Install plugin
     cd "${_CURRENT_DIR}wp-content/mu-plugins/wpu/";
-    git submodule add --force "https://github.com/WordPressUtilities/${_MUPLUGIN_ID}.git";
+    git submodule add --force "${_MUPLUGIN_REPO_URL}.git";
 
     # Commit items
     cd "${_CURRENT_DIR}";
